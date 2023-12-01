c9f52780ee56b49839ba73d2bd1b067cc3f6e52e
Aluno <Aluno@politecnico.local>
2023-12-01 14:47:48 -0300

    Envio Final

--- home/models.py
+++ home/models.py
@@ -4,7 +4,7 @@ from stdimage import StdImageField
 
 class Pessoa(models.Model):
     nome = models.CharField('Nome', max_length = 50, help_text = 'Nome completo')
-    email = models.CharField('Email', max_length=15, help_text='Numero de Telefone')
+    email = models.EmailField('Email', max_length=100, help_text='Numero de Telefone')
     foto = StdImageField('Foto', upload_to='pessoas', delete_orphans=True, null=True, blank=True)
 
     class Meta:
--- topicos/settings.py
+++ topicos/settings.py
@@ -71,8 +71,12 @@ WSGI_APPLICATION = 'topicos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'chave',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
